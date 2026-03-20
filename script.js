3910094701e084363467b918b16cff48c010a018
Ritaa-Marie <ritamariecajuru@gmail.com>
2026-03-19 21:54:48 -0300

    Início das funções de validação.

---
+++ script.js
@@ -0,0 +1,37 @@
+// validação das entradas de dados
+
+
+function validarNomeCompleto(nome){
+    let validar = /^[A-Za-zÀ-ÿ\s]+$/.test(nome);
+    if (nome.trim() === ""){
+        return false
+    }
+    if (!validar){
+        return false
+    } else {
+        return true
+    }
+}
+
+function validarCEP(numero){
+    let cep = numero.match(/\+d/g);
+    if (cep.legth !== 8){
+        return false;
+    } else {
+        return true
+    }
+}
+
+function validarTelefone(telefone){
+    let numero = telefone.match(/\+d/g);
+    if (numero.legth !== 11){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+
+// leitura de variáveis
+let nome = document.getElementById("nome")
