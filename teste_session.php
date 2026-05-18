00e6630f872e9ddccb6cff0e2c85cdce3c0337aa
Ritaa-Marie <ritamariecajuru@gmail.com>
2026-05-18 19:49:46 -0300

    Excluindo session para testes.

--- teste_session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$_SESSION['admin'] = 1;      // coloca o id de um usuário que existe no banco
-$_SESSION['usuario']    = 'rita'; // o nome do usuário
-$_SESSION['tipo']       = 'usuario'; // 'usuario' ou 'admin'
-
-header("Location: clientes.php");
-exit;
